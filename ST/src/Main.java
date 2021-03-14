661229d7dfeedd6fb8cf184487bfeafbeb177749
kewpf <kewpf@DESKTOP-F8DVBEC>
2021-03-14 09:10:12 +0300

    Ferst commiet

---
+++ ST/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+ST s =new ST();
+s.setName("Ali");
+s.setAge(24);
+    System.out.println(s.getName());
+    System.out.println(s.getAge());
+
+    }
+
+}
